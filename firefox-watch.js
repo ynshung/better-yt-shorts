52d349c20776163f1850f535390416157ad3b192
Young Shung <me@ynshung.com>
2023-09-17 20:11:46 +0800

    patch v3 dev for firefox

---
+++ firefox-watch.js
@@ -0,0 +1,18 @@
+const nodemon = require('nodemon');
+const fs = require('fs');
+
+const FILES_TO_WATCH = "ts,tsx,scss";
+
+const watcher = nodemon({
+  ext: FILES_TO_WATCH,
+  script: 'firefox-fix-manifest.js',
+});
+
+watcher.on('quit', () => {
+  console.log('Stopped watching files.');
+  process.exit();
+});
+
+watcher.on('restart', (files) => {
+  console.log(`File changes detected: ${files.join(', ')}`);
+});
