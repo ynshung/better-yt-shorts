bc9cf3971a95da4254f61f06897f4c779756d0ff
Young Shung <me@ynshung.com>
2023-10-26 02:08:47 +0800

    format and lint

--- src/lib/handleKeyEvent.ts
+++ src/lib/handleKeyEvent.ts
@@ -1,9 +1,13 @@
-import { setHideShortsOverlay } from "./HideShortsOverlay"
-import { goToNextShort, goToPreviousShort, restartShort } from "./VideoState"
-import { setVolume } from "./VolumeSlider"
-import { VOLUME_INCREMENT_AMOUNT } from "./declarations"
-import { BooleanDictionary, PolyDictionary, StringDictionary } from "./definitions"
-import { getVideo } from "./getters"
+import { setHideShortsOverlay } from "./HideShortsOverlay";
+import { goToNextShort, goToPreviousShort, restartShort } from "./VideoState";
+import { setVolume } from "./VolumeSlider";
+import { VOLUME_INCREMENT_AMOUNT } from "./declarations";
+import {
+  BooleanDictionary,
+  PolyDictionary,
+  StringDictionary,
+} from "./definitions";
+import { getVideo } from "./getters";
 
 export function handleKeyEvent(
   e: KeyboardEvent,
@@ -11,17 +15,22 @@ export function handleKeyEvent(
   keybinds: StringDictionary,
   settings: any,
   options: PolyDictionary,
-  state: any
+  state: any,
 ) {
   if (
-    [ ...document.querySelectorAll("input") ].includes( document.activeElement as HTMLInputElement ) ||
-    [ ...document.querySelectorAll("#contenteditable-root") ].includes( document.activeElement as HTMLElement )
-  ) return // Avoids using keys while the user interacts with any input, like search and comment.
+    [...document.querySelectorAll("input")].includes(
+      document.activeElement as HTMLInputElement,
+    ) ||
+    [...document.querySelectorAll("#contenteditable-root")].includes(
+      document.activeElement as HTMLElement,
+    )
+  )
+    return; // Avoids using keys while the user interacts with any input, like search and comment.
 
-  if ( features !== null && !features[ "keybinds" ] ) return
+  if (features !== null && !features["keybinds"]) return;
 
-  const ytShorts = getVideo()
-  if ( !ytShorts ) return
+  const ytShorts = getVideo();
+  if (!ytShorts) return;
 
   const key = e.code;
   const keyAlt = e.key.toLowerCase(); // for legacy keybinds
@@ -39,46 +48,52 @@ export function handleKeyEvent(
 
   if (!command) return;
 
-  const volumeSliderEnabled = features !== null && features[ "volumeSlider" ]
+  const volumeSliderEnabled = features !== null && features["volumeSlider"];
 
   switch (command) {
     case "seekBackward":
-      ytShorts.currentTime -= options?.seekAmount
-      break
+      ytShorts.currentTime -= options?.seekAmount;
+      break;
 
     case "seekForward":
-      ytShorts.currentTime += options?.seekAmount
-      break
+      ytShorts.currentTime += options?.seekAmount;
+      break;
 
     case "decreaseSpeed":
-      if (ytShorts.playbackRate > 0.25) ytShorts.playbackRate -= 0.25
-      break
+      if (ytShorts.playbackRate > 0.25) ytShorts.playbackRate -= 0.25;
+      break;
 
     case "resetSpeed":
-      ytShorts.playbackRate = 1
-      break
+      ytShorts.playbackRate = 1;
+      break;
 
     case "increaseSpeed":
-      if ( ytShorts.playbackRate < 16 ) ytShorts.playbackRate += 0.25
-      break
+      if (ytShorts.playbackRate < 16) ytShorts.playbackRate += 0.25;
+      break;
 
     case "increaseVolume":
-      if ( ytShorts.volume < 1 )
-        setVolume( settings, ytShorts.volume + VOLUME_INCREMENT_AMOUNT, volumeSliderEnabled )
+      if (ytShorts.volume < 1)
+        setVolume(
+          settings,
+          ytShorts.volume + VOLUME_INCREMENT_AMOUNT,
+          volumeSliderEnabled,
+        );
 
-      if ( ytShorts.volume > 1 )
-        ytShorts.volume = 1
+      if (ytShorts.volume > 1) ytShorts.volume = 1;
 
-      break
+      break;
 
     case "decreaseVolume":
-      if ( ytShorts.volume > 0 )
-        setVolume( settings, ytShorts.volume - VOLUME_INCREMENT_AMOUNT, volumeSliderEnabled )
+      if (ytShorts.volume > 0)
+        setVolume(
+          settings,
+          ytShorts.volume - VOLUME_INCREMENT_AMOUNT,
+          volumeSliderEnabled,
+        );
 
-      if ( ytShorts.volume < 0 )
-        ytShorts.volume = 0
+      if (ytShorts.volume < 0) ytShorts.volume = 0;
 
-      break
+      break;
 
     // case "toggleMute":
     //   if ( !state.muted )
@@ -96,34 +111,35 @@ export function handleKeyEvent(
 
     case "previousFrame":
       if (ytShorts.paused) {
-        ytShorts.currentTime -= 0.04
+        ytShorts.currentTime -= 0.04;
       }
-      break
+      break;
 
     case "nextFrame":
       if (ytShorts.paused) {
-        ytShorts.currentTime += 0.04
+        ytShorts.currentTime += 0.04;
       }
-      break
+      break;
 
     case "nextShort":
-      goToNextShort()
-      break
+      goToNextShort();
+      break;
 
     case "previousShort":
-      goToPreviousShort()
-      break
+      goToPreviousShort();
+      break;
 
     case "restartShort":
-      restartShort()
-      break
+      restartShort();
+      break;
   }
 
-  state.playbackRate = ytShorts.playbackRate
+  state.playbackRate = ytShorts.playbackRate;
 
   function validateKeybind(keybind: string) {
     const _split = keybind.split(".");
-    if (mod) return _split.length === 2 && _split[0] === mod && key === _split[1];
+    if (mod)
+      return _split.length === 2 && _split[0] === mod && key === _split[1];
     return key === keybind || keyAlt === keybind;
   }
 }
