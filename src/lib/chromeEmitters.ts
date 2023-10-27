7d7fe1c371b4e8542010b3e21885cfad7ec5526c
Young Shung <me@ynshung.com>
2023-10-28 00:35:58 +0800

    restore pinging

--- src/lib/chromeEmitters.ts
+++ src/lib/chromeEmitters.ts
@@ -1,4 +1,4 @@
-// import BROWSER from "../background/browser";
+import BROWSER from "../background/browser";
 import { ChangedObjectStateEnum } from "./definitions";
 
 /**
@@ -12,16 +12,16 @@ export function pingChanges(
   message: object,
 ) {
   (async () => {
-    // const [tab] = await BROWSER.tabs.query({
-    //   active: true,
-    //   lastFocusedWindow: true,
-    // });
+    const [tab] = await BROWSER.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+    });
     const key = ChangedObjectStateEnum[objectEnum].toLowerCase() ?? null;
 
     const content: { [key: string]: object } = {};
     content[key] = message;
 
-    // const response = await BROWSER.tabs.sendMessage(tab.id as number, content); // ! - see if this works in firefox
+    await BROWSER.tabs.sendMessage(tab.id as number, content); // ! - see if this works in firefox
 
     // do something with response here, not outside the function
     console.log(`[BYS] :: Saving Changes`);
